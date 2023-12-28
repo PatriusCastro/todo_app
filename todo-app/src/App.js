508ba71efe068da7dc488bae98e2e5dfdfcd5231
Pat Castro <josuahcastro1@gmail.com>
2023-12-28 22:22:05 +0800

    structured

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,20 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Todos</h1>
+
+      <div className="todo_wrapper">
+        <div className="todo_input">
+          <div className="todo_input_item">
+            <label>Title</label>
+            <input type="text" placeholder="Enter your task title"></input>
+          </div>
+          <div className="todo_input_item">
+            <label>Description</label>
+            <input type="text" placeholder="Enter your task description"></input>
+          </div>
+          <div className="todo_input_item">
+            <button type='button' className="primaryBtn">Add</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
