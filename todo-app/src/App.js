200be69ec2c73c2b8bd6a81d2977eea72cecaf18
Pat Castro <josuahcastro1@gmail.com>
2023-12-30 20:54:29 +0800

    first structure design done

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,23 +4,46 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My Todos</h1>
-
+        
+    
       <div className="todo_wrapper">
+        
+        <h1>My Todos</h1>
+
         <div className="todo_input">
+
           <div className="todo_input_item">
             <label>Title</label>
             <input type="text" placeholder="Enter your task title"></input>
           </div>
+
           <div className="todo_input_item">
             <label>Description</label>
             <input type="text" placeholder="Enter your task description"></input>
           </div>
+
           <div className="todo_input_item">
             <button type='button' className="primaryBtn">Add</button>
           </div>
+
+        </div> 
+
+        <div className="list_container">
+          <div className="btn_area">
+            <button>Todos</button>
+            <button>Completed</button>
+          </div>
+
+          <div className="todo_list">
+            <div className="todo-list-item">
+              <h3>Task 1</h3>
+              <p>Description</p>
+            </div>
+          </div>
         </div>
+
       </div>
+
     </div>
   );
 }
